91fcf4d5a8ea98037ec74f8cd6f46ad7243fc71f
Andrew Kim <akim219@aucklanduni.ac.nz>
2025-08-03 12:56:31 +1200

    Linting fixed

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,41 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import eslintPluginImport from "eslint-plugin-import";
+import typescript from "typescript-eslint";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.ts", "**/*.tsx"],
+    ...typescript.config(),
+    plugins: {
+      prettier,
+      react,
+      "react-hooks": reactHooks,
+      import: eslintPluginImport,
+    },
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parser: typescript.parser,
+      // Remove the project reference or fix the path
+      parserOptions: {
+        // project: "./tsconfig.json",
+      },
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        document: "readonly",
+        window: "readonly",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "prettier/prettier": "error",
     },
   },
-])
+];
